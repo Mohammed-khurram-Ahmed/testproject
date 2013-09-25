2ba7e425a075ec1035f92c5b488b5b232f20ea2f
Khurram <Khurram@Khurram-PC>
2013-09-25 17:56:38 +0530

    Ghansham Commits

--- SampleApp/src/com/example/sampleapp/MainActivity.java
+++ SampleApp/src/com/example/sampleapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.sampleapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +11,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	Toast.makeText(getApplicationContext(), "Hi Ghansham Here", 1).show();
+	
 	}
 
 	@Override
